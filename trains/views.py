a2bd5385bf97d597543fc52628b5f3bc649672dd
Oleg <jf2.oleg@gmail.com>
2026-01-10 19:44:37 +0200

    Додав відображення списку та деталізації Поїздів

--- trains/views.py
+++ trains/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView,
+)
 
-# Create your views here.
+# from trains.forms import TrainForm
+from trains.models import Train
+
+__all__ = (
+    'TrainListView', 'TrainDetailView', 'TrainCreateView', 'TrainUpdateView',
+    'TrainDeleteView'
+)
+
+
+class TrainListView(ListView):
+    model = Train
+    template_name = 'trains/home.html'
+    paginate_by = 2
+
+
+class TrainDetailView(DetailView):
+    model = Train
+    template_name = 'trains/detail.html'
+
+
+class TrainCreateView(SuccessMessageMixin, CreateView):
+    model = Train
+    template_name = 'trains/create.html'
+    # form_class = TrainForm
+    success_url = reverse_lazy('trains:home')
+
+    def get_success_message(self, cleaned_data):
+        message = 'Поїзд {name} успішно створено'
+        return message.format(**cleaned_data)
+
+class TrainUpdateView(SuccessMessageMixin, UpdateView):
+    model = Train
+    template_name = 'trains/update.html'
+    # form_class = TrainForm
+    success_url = reverse_lazy('trains:home')
+
+    def get_success_message(self, cleaned_data):
+        message = 'Поїзд {name} успішно відредаговано'
+        return message.format(**cleaned_data)
+
+
+class TrainDeleteView(DeleteView):
+    model = Train
+    template_name = 'trains/delete.html'
+    success_url = reverse_lazy('trains:home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        success_message = self.get_success_message({'name': self.object.name})
+        if success_message:
+            messages.success(self.request, success_message)
+        return response
+
+    def get_success_message(self, cleaned_data):
+        message = 'Поїзд {name} успішно видалено'
+        return message.format(**cleaned_data)
--- travelling/urls.py
+++ travelling/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cities/', include(('cities.urls', 'cities')))
+    path('cities/', include(('cities.urls', 'cities'))),
+    path('trains/', include(('trains.urls', 'trains')))
     # path('home/', home),
-    # path('about/', about),
 ]
