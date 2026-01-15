1f60836248113ebe68a3d36855b236c5a70318a8
Oleg <jf2.oleg@gmail.com>
2026-01-15 19:41:35 +0200

    форма для пошуку маршруту RouteForm

---
+++ routes/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from cities.models import City
+from trains.models import Train
+
+
+class RouteForm(forms.Form):
+    expected_time = forms.DecimalField(
+        label='Час у дорозі', widget=forms.NumberInput(
+            attrs={'class': 'form-control', }
+        ))
+    from_city = forms.ModelChoiceField(
+        label='З якого міста', queryset=City.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control', })
+    )
+    to_city = forms.ModelChoiceField(
+        label='До якого міста', queryset=City.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control', })
+    )
+    cities = forms.ModelMultipleChoiceField(
+        label='Через міста', queryset=City.objects.all(),
+        required=False,
+        widget=forms.SelectMultiple(attrs={'class': 'form-control', })
+    )
