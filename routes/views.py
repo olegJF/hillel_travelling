60b5f0f6379e0d173b9661783fb73a8a1e5b0429
Oleg <jf2.oleg@gmail.com>
2026-01-16 21:03:40 +0200

    Додав функцію відображення find_routes

--- routes/views.py
+++ routes/views.py
@@ -1,8 +1,24 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from routes.forms import RouteForm
+
+__all__ = ('home', 'find_routes')
 
 
 def home(request):
     form = RouteForm()
     return render(request, 'routes/home.html', {'form': form})
+
+
+def find_routes(request):
+    if request.method == 'POST':
+        form = RouteForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            a = 1
+        return render(request, 'routes/home.html', {'form': form})
+    else:
+        messages.error(request, 'Немає даних для пошуку')
+        return HttpResponseRedirect('/')
--- travelling/urls.py
+++ travelling/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from routes.views import home
+from routes.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
     path('', home, name='home'),
+    path('find_routes/', find_routes, name='find_routes'),
 ]
