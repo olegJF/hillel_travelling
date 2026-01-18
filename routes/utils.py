702a359826125e85d2b1b463574eaf586ed806c0
Oleg <jf2.oleg@gmail.com>
2026-01-18 16:13:31 +0200

    Функція пошуку маршрутів у графі dfs_paths

--- routes/utils.py
+++ routes/utils.py
@@ -1,6 +1,21 @@
 from trains.models import Train
 
 
+def dfs_paths(graph, start, goal):
+    """
+    Функція пошуку усіх можливих маршрутів з одного міста в інше.
+    """
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        if vertex in graph.keys():
+            for next_ in graph[vertex] - set(path):
+                if next_ == goal:
+                    yield path + [next_]
+                else:
+                    stack.append((next_, path + [next_]))
+
+
 def get_graph() -> dict:
     graph = {}
     qs = Train.objects.all()
@@ -11,4 +26,11 @@ def get_graph() -> dict:
 
 def get_all_routes(request, form) -> dict:
     graph = get_graph()
-    raise ValueError('Немає даних для пошуку')
+    context = {'form': form}
+    data = form.cleaned_data
+    from_city = data['from_city']
+    to_city = data['to_city']
+    all_ways = list(dfs_paths(graph, from_city.id, to_city.id))
+    if not all_ways:
+        raise ValueError('Маршруту, що задовільняє цим вимогам не існує')
+    return context
