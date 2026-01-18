015f32ea40d7dbb48ecb54f2edd2fcbf9f75194f
Oleg <jf2.oleg@gmail.com>
2026-01-18 15:32:57 +0200

    Функція побудови графу get_graph

--- routes/utils.py
+++ routes/utils.py
@@ -1,4 +1,14 @@
+from trains.models import Train
 
+
+def get_graph() -> dict:
+    graph = {}
+    qs = Train.objects.all()
+    for q in qs:
+        graph.setdefault(q.from_city_id, set())
+        graph[q.from_city_id].add(q.to_city_id)
+    return graph
 
 def get_all_routes(request, form) -> dict:
+    graph = get_graph()
     raise ValueError('Немає даних для пошуку')
